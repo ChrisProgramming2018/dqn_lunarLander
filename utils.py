ed575d04fab04e7ca0dfa0f155659dd3cbfbde89
Programmer <info2016frei@gmail.com>
2021-01-30 09:13:31 +0100

    mode

--- utils.py
+++ utils.py
@@ -1,5 +1,18 @@
+import os
 
 
+def mkdir(base, name):
+    """
+    Creates a direction if its not exist
+    Args:
+       param1(string): base first part of pathname
+       param2(string): name second part of pathname
+    Return: pathname
+    """
+    path = os.path.join(base, name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
 
 def time_format(sec):
     """
